4305a18a52d74147f4ba22c886703fbeee2d89d8
Tu Nombre <correo@ejemplo.com>
2025-08-26 00:26:30 -0500

    Nuevo Par de claves

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Mi proyecto
 - Primer cambio: Crear un archivo main.py
 - Segundo cambio: Añadir linea al archivo main.py
+- Tercer cambio: Par de claves ssh creadas  
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 print("Este es mi primer proyecto con Git")
 print("Otro cambio para el tercer commit de mi repositorio con Git")
+print("Ya tengo mi par de claves ssh")
 print('Hola Mundo')
 
